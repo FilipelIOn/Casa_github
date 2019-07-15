345da784a53d1fdba0e1cb427ef65430fe5ead10
filipe <filipe.freitas@outlook.com>
2019-07-15 18:35:22 -0300

    deletados

--- Contests/Roteiro UFMG/Roteiro 0/FATORIA2.py
+++
@@ -1,6 +0,0 @@
-n = int(input())
-fat = 1; i = 2
-while i <= n:
-    fat = fat*i
-    i+= 1
-print(fat)
--- Contests/Roteiro UFMG/Roteiro 0/JPNEU.py
+++
@@ -1,3 +0,0 @@
-pressao_indicada = int(input())
-pressao_lida = int(input())
-print(pressao_indicada - pressao_lida)
